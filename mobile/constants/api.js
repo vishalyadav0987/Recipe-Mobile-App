887ff421eb69c80fd8bff6a89b7a23fa9d7a318e
vishalyadav0987 <mr.yadavvishal0987@gmail.com>
2025-07-03 19:43:28 +0530

    feat(mobile): initialize mobile app with Expo, add meal API service, and configure TypeScript

---
+++ mobile/constants/api.js
@@ -0,0 +1,3 @@
+// if you're using your physical phone, change this to the deployed url
+// we have explained this in the course :-)
+export const API_URL = "https://recipe-mobile-app-t7d6.onrender.com/api/v1";
